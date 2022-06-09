f774df66a5026cd91d9f4735ebc1cfd9f4314bd8
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-09 09:40:17 +0300

    commit tree 5a73958ea1458417365d31686701a4d8ed2390fc

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+// writeCards()
+//       1) returns an array of thank you messages for each name provided to the function
+//     countDown()
+//       2) invokes console.log once for each number, counting down from the number provided to zero
+//       3) logs each number when counting down, starting from the number provided
+
+const writeCards = (names) => {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+const countDown = (num) => {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
